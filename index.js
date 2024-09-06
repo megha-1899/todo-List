5e9c69eaca711ba5d5b9ee8718cfce27c93476ab
Megha-Padsalgi <meghapadsalgi@gmail.com>
2024-09-06 18:06:01 +0530

    Added the files

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const TODOModel = require('./Models/Todo')
+import express, { json } from 'express'
+import { connect } from 'mongoose'
+import cors from 'cors'
+import { find, findByIdAndUpdate, findByIdAndDelete, create } from './Todo'
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(json())
 
-mongoose.connect('mongodb+srv://padsalgimegha:WRXFYuyKNmoFZora@cluster0.xiyqr.mongodb.net/Todo')
+connect('mongodb+srv://padsalgimegha:WRXFYuyKNmoFZora@cluster0.xiyqr.mongodb.net/Todo')
 
 app.get('/get', (req,res) => {
-    TODOModel.find()
+    find()
     .then(result => res.json(result))
     .catch(err => res.json(err))
 })
 
 app.put('/update/:id', (req,res) => {
     const {id} = req.params;
-    TODOModel.findByIdAndUpdate({_id: id}, {done: true})
+    findByIdAndUpdate({_id: id}, {done: true})
     .then(result => res.json(result))
     .catch(err => res.json(err))
 })
 
 app.delete('/delete/:id', (req,res) => {
     const {id} = req.params;
-    TODOModel.findByIdAndDelete({_id: id})
+    findByIdAndDelete({_id: id})
     .then(result => res.json(result))
     .catch(err => res.json(err))
 })
 
 app.post('/add', (req,res) => {
     const task = req.body.task;
-    TODOModel.create({
+    create({
         task: task
     }).then(result => res.json(result))
     .catch(err => res.json(err))
