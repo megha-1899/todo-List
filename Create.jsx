6346ebb0677899cee0d62a79e52cf1b73945f58e
megha-1899 <73352629+megha-1899@users.noreply.github.com>
2024-09-06 18:27:08 +0530

    Update Create.jsx

--- Create.jsx
+++ Create.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 
 function Create() {
-  const [task, setTask] = useState()
+  const [task, setTask] = useState();
   const handleAdd = () => {
     axios.post('http://localhost:3001/add', {task: task})
     .then(result => {
@@ -13,7 +13,7 @@ function Create() {
 
   return (
     <div>
-        <input className="input" type="text" placeholder="Enter To Do" onChange={(e) = setTask(e.target.value)} />
+        <input className="input" type="text" placeholder="Enter To Do" onChange={(e) => setTask(e.target.value)} />
         <button className="button" type='button' onClick={handleAdd} >Submit</button>
     </div>
   )
